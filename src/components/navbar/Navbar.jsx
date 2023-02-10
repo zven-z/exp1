622559af44b7124b6dd426c19f5eb3b0f294b27d
zven-z <latiasimi@gmail.com>
2023-02-10 15:48:24 -0500

    add logo to navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './navbar.css';
+
+import logo from '../../assets/logo.png';
+import './navbar.css';
+// BEM => block element modifier
+
 const Navbar = () => {
   return (
-    <div>
-       Navbar
+    <div className='club-navbar'>
+        <div className='club_navbar-links'>
+          <div className='club_navbar-links_logo'>
+            <img src={logo} alt="logo"/>
+          </div>
+
+        </div>
     </div>
   )
 }
